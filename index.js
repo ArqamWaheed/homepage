04bd42d2a604d6a1f5010d9aba4ed7275e7f6dfb
ArqamWaheed <86839084+ArqamWaheed@users.noreply.github.com>
2025-10-19 21:48:25 +0500

    Added navbar functionality js left

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 $body = document.querySelector("body");
-$themeSvg = document.querySelector("nav svg");
+$themeSvg = document.querySelector(".lightdark-icon");
 
 $themeSvg.addEventListener('click', function() {
     $body.classList.toggle("light");
